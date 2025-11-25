b423622988f87b469f47184a4497959f4364dad9
aarkaymedes <rkiran@pratt.edu>
2025-11-25 17:59:52 -0500

    Changed audio wave

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ const KEY_MIDI_OFFSETS = {
 // --- FEATURE/SOUND VARIABLES ---
 let chordOscillators = []; 
 const MAX_VOICES = 4;
-const SYNTH_WAVEFORM = 'sawtooth';
+const SYNTH_WAVEFORM = 'sine';
 
 // --- SLIDER VARIABLES ---
 let sliderPos = 0.5; 
